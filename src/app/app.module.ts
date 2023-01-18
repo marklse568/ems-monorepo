7be034ec83294eb861197da4e0dcb61fb40b1648
Me Me <me@just-wanna.dev>
2023-01-18 10:05:14 +0100

    keycloak init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
-import {NgModule} from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import {KeycloakAngularModule, KeycloakService} from "keycloak-angular";
+import {initializeKeycloak} from "./keycloak-init.factory";
 
 @NgModule({
   declarations: [
@@ -14,9 +16,17 @@ import { EmployeeListComponent } from './employee-list/employee-list.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    KeycloakAngularModule
   ],
-  providers: [],
+  providers: [
+  {
+    provide: APP_INITIALIZER,
+    useFactory: initializeKeycloak,
+    multi: true,
+    deps: [KeycloakService],
+  }
+],
   bootstrap: [AppComponent]
 })
 export class AppModule {
---
+++ src/app/keycloak-init.factory.ts
@@ -0,0 +1,19 @@
+import { KeycloakService } from "keycloak-angular";
+
+export function initializeKeycloak(
+  keycloak: KeycloakService
+) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'https://keycloak.szut.dev/auth',
+        realm: 'szut',
+        clientId: 'employee-management-service-frontend',
+      },
+      initOptions: {
+        checkLoginIframe: true,
+        checkLoginIframeInterval: 25
+      },
+      loadUserProfileAtStartUp: true
+    });
+}
