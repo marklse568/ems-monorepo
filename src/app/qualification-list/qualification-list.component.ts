cab16c947e4b036d87c244f037a00976bd518458
Mark L <marklse568@gmail.com>
2023-01-19 19:58:10 +0100

    Refactor data fetching to best practices

--- src/app/qualification-list/qualification-list.component.ts
+++ src/app/qualification-list/qualification-list.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
 import { EmployeeApiService } from '../service/employee-api.service';
 import { Qualification } from '../model/Qualification';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-qualification-list',
@@ -10,27 +8,22 @@ import { Router } from '@angular/router';
   styleUrls: ['./qualification-list.component.css'],
 })
 export class QualificationListComponent implements OnInit {
-  qualifications$: Observable<Qualification[]>;
-  newName: string;
+  qualifications: Qualification[] = [];
+  newName = '';
 
-  constructor(private restService: EmployeeApiService, private router: Router) {
-    this.qualifications$ = of([]);
-    this.newName = '';
-  }
+  constructor(private restService: EmployeeApiService) {}
 
   ngOnInit() {
-    this.qualifications$ = this.restService.getAllQualifications();
+    this.restService.getAllQualifications().subscribe((data) => {
+      this.qualifications = [...data];
+    });
   }
 
   save() {
     this.restService
       .addQualification(new Qualification(this.newName))
-      .toPromise()
-      .then((q) => {
-        this.qualifications$ = this.restService.getAllQualifications();
-      })
-      .catch(() => {
-        console.error('failed to add qualification');
+      .subscribe((data) => {
+        this.qualifications = [...this.qualifications, data];
       });
   }
 }
