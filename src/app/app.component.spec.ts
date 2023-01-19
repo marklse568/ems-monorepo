17227fc84f7efbb4a96cecab539b000a7097344c
Mark Loose <m.loose@boewa.de>
2023-01-19 11:07:37 +0100

    Update project name and window title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,10 +14,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'employeeFrontendStarter'`, () => {
+  it(`should have as title 'EMS'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('employeeFrontendStarter');
+    expect(app.title).toEqual('EMS');
   });
 
   it('should render title', () => {
@@ -25,7 +25,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'employeeFrontendStarter app is running!'
+      'EMS app is running!'
     );
   });
 });
